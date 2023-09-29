075cb3fb216bb4848e40c61b0b70f99fb4394a49
Renan G. Silva <renan.amigos28@gmail.com>
2023-09-29 09:06:49 -0300

    feat: Busca de users no github, usando guzzle http

--- searchGitUsers.php
+++ searchGitUsers.php
@@ -1,13 +1,15 @@
 <?php
 
-require 'vendor\autoload.php';
+require './vendor/autoload.php';
 
 use GuzzleHttp\Client;
-use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client();
 
 $res = $client->request('GET', 'https://api.github.com/users');
 $html = $res->getBody();
-$dowCrawler = new Crawler($html);
-var_dump($res);
+
+
+foreach (json_decode($html) as $user) {
+    echo $user->login . PHP_EOL;
+}
